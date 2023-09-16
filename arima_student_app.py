852480865443e96006cb3b35d2af77d2316812e5
Jaime Pizarroso Gonzalo <jpizarroso@comillas.edu>
2023-09-16 18:19:38 +0200

    [STUDENT]: Distinguish student and professor dashboard, delete solutions related buttons.

--- arima_student_app.py
+++ arima_student_app.py
@@ -4,9 +4,6 @@ from arima_config import config_function
 from google.oauth2 import service_account
 import gspread
 from datetime import datetime
-import bar_chart_race as bcr
-import streamlit.components.v1 as components
-import base64
 
 # Make the web page fill the full area
 st.set_page_config(layout="wide")
@@ -117,11 +114,6 @@ def read_series():
     
     return series_df, error_flag
 
-# Function to clear the session state for showing solutions
-def clear_show_solution():
-    st.session_state.show_solution = False
-    return
-
 
 def main():
     # Set the main title based on the problem type from the config file
@@ -145,8 +137,7 @@ def main():
     # Dropdown for selecting Series number
     option = st.sidebar.selectbox(
         'Series #',
-        opt,
-        on_change=clear_show_solution
+        opt
     )
 
     # Initialize session state for results if not already present
@@ -160,13 +151,6 @@ def main():
             df_columns = ['Team', 'p', 'd', 'q', 'P', 'D', 'Q', 'f']
 
         st.session_state.results = pd.DataFrame(columns=df_columns)
-
-    # Initialize session state for showing solution
-    if 'show_solution' not in st.session_state:
-        st.session_state.show_solution = False
-    
-    if 'show_results' not in st.session_state:
-        st.session_state.show_results = False
 
     # Sidebar Buttons
     # Submit Button
@@ -220,25 +204,6 @@ def main():
                     st.sidebar.error(f"Error: {team_name} has exceeded the number of tries for Serie {option}.")
             else:
                 st.sidebar.error("Error: Incorrect user or password.")
-                
-    # Solution area
-    # Button for showing solution
-    if st.sidebar.button("Show solution"):
-        # Admin check for showing solution
-        if team_name == admin_account['name'] and password == admin_account['password']:
-            st.session_state.show_solution = True
-        else:
-            st.session_state.show_solution = False
-            st.sidebar.error("Error: wrong admin name or password")
-    
-    # Button for showing results
-    if st.sidebar.button('Show results'):
-        # Admin check for showing results
-        if team_name == admin_account['name'] and password == admin_account['password']:
-            st.session_state.show_results = True
-        else:
-            st.session_state.show_results = False
-            st.sidebar.error("Error: wrong admin name or password")
             
     # Main area
     st.title("Input parameters")
@@ -248,68 +213,6 @@ def main():
     for index, col in enumerate(st.session_state.results.columns[1:]):
         with txtColumns[index]:
             st.text_input(col, key=col)
-        
-    
-    if st.session_state.show_solution:
-        series_solution = series.loc[series['Series'] == option, st.session_state.results.columns[1:]]
-        st.dataframe(pd.DataFrame(series_solution))
-    
-    if st.session_state.show_results:
-        previous_results, _, _ = read_previous_results()
-        
-        # Convert time to datetime
-        previous_results['Time'] = pd.to_datetime(previous_results['Time'])
-        
-        # Merge previous results with the series DataFrame on specific columns
-        # This combines the 'Series', 'p', and 'q' columns from both DataFrames
-        merged_df = pd.merge(previous_results.reset_index(), series, 
-                            on=['Series', 'p', 'q'], how='left')
-
-        print(previous_results)
-        print(merged_df)
-        # Fill any NaN values in the 'weight' column with zeros
-        # This ensures that all teams have a 'weight' even if they haven't made any attempts
-        merged_df['weight'].fillna(0, inplace=True)
-
-        # Create a 'mark' column to hold the cumulative sum of 'weight' for each team
-        # This will be used for scoring, and is multiplied by 100 for percentage representation
-        merged_df['mark'] = merged_df.groupby('Team')['weight'].cumsum() * 100
-
-        # Re-set the index of the merged DataFrame to 'Time'
-        # This is to ensure that the DataFrame is indexed by time, which is likely important for time-series data
-        merged_df.set_index('Time', inplace=True)
-
-        # Number of steps for expanding the DataFrame index
-        n_steps = config_file['n_steps']
-
-        # Filter the DataFrame to include only 'Team' and 'mark' columns
-        df = merged_df[['Team', 'mark']]
-
-        # Sort by 'mark' in descending and 'Time' in ascending order
-        sorted_df = merged_df.sort_values(by=['mark', 'Time'], ascending=[False, True])
-
-        # Get the team with the highest mark that reached it the fastest
-        winning_team = sorted_df.iloc[0]['Team']
-        
-        # Pivot the DataFrame so that each 'Team' becomes a column
-        df = df.pivot(columns='Team', values='mark')
-        
-        # Create bar chart race animation
-        html_str = bcr.bar_chart_race(
-                df.ffill().fillna(0), 
-                interpolate_period=True, steps_per_period=n_steps, title='Team leaderboard evolution',
-                period_fmt='%H:%m'
-            ).data
-
-        start = html_str.find('base64,') + len('base64,')
-        end = html_str.find('">')
-
-        video = base64.b64decode(html_str[start:end])
-        
-        st.video(video)
-        
-        # Show winning team name
-        st.subheader(f"Winning team: {winning_team}")
         
 
 if __name__ == "__main__":
